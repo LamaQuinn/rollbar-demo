a2d229b086c30d0ffec5cc32f2f04d9ad6f2d86b
Laman Ismayilova <laman.mamedova98@gmail.com>
2023-08-17 10:11:32 -0700

    added rollbar func

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/students', (req, res) => {
+    rollbar.info('Someone access the list of students.')
     res.status(200).send(students)
 })
 
@@ -34,19 +35,24 @@ app.post('/api/students', (req, res) => {
    try {
        if (index === -1 && name !== '') {
            students.push(name)
+           rollbar.info("Someone created a new student. Wowza")
            res.status(200).send(students)
        } else if (name === ''){
+        rollbar.error("Someone submit a blank name! Stop that!")
            res.status(400).send('You must enter a name.')
        } else {
+        rollbar.error("Someone tried to clone a student. Nono")
            res.status(400).send('That student already exists.')
        }
    } catch (err) {
+        rollbar.error(err)
        console.log(err)
    }
 })
 
 app.delete('/api/students/:index', (req, res) => {
     const targetIndex = +req.params.index
+    rollbar.warning("Someone deleted a student.")
     
     students.splice(targetIndex, 1)
     res.status(200).send(students)
